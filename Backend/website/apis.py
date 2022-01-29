e74d3b5be81174dbf8a27fbb55f2a2dfa5833946
Shamal Jayathilake <54285211+shamaljayathilake@users.noreply.github.com>
2022-01-30 02:53:16 +0530

    working Nice

--- Backend/website/apis.py
+++ Backend/website/apis.py
@@ -162,36 +162,35 @@ def addFlow():
 @login_required
 def removeFlow():
     if request.method == 'POST':
-        dstIP = request.form.get('dstIP')
         switch = request.form.get("switch")
-        if switch == "0":
-            for sw in range (1,8,1):
-                tempData = copy.deepcopy(jsonData)
-                tempData['input']["match"]["ipv4-destination"] = str(dstIP)
-                tempData['input']["node"]= "/opendaylight-inventory:nodes/opendaylight-inventory:node[opendaylight-inventory:id='openflow:"+ str(sw) +"']"
-                sendData = json.dumps(tempData)
-                response = requests.post(removeFlowUrl,auth=HTTPBasicAuth('admin', 'admin'), data=sendData,headers=headers)
-            if response.status_code ==200:
-                flash('Flow Removed', category='success')
-                return redirect(url_for('views.postPage'))
-            else:
-                flash('Flow Removing Error', category='error')
-                return redirect(url_for('views.postPage'))
+        tempData = copy.deepcopy(jsonData)
+        tempData['input']["priority"] = request.form.get('priority')
+        if request.form.get('action') == "Allow":
+            tempData['input']["instructions"]["instruction"][0]["apply-actions"]["action"]= [{"order": 0,"output-action": {"output-node-connector": "ALL","max-length": 60}}]
         else:
-            tempData = copy.deepcopy(jsonData)
-            tempData['input']["match"]["ipv4-destination"] = str(dstIP)
-            tempData['input']["node"]= "/opendaylight-inventory:nodes/opendaylight-inventory:node[opendaylight-inventory:id='openflow:"+ str(switch) +"']"
-            sendData = json.dumps(tempData)
-            response = requests.post(removeFlowUrl,auth=HTTPBasicAuth('admin', 'admin'), data=sendData,headers=headers)
-            if response.status_code ==200:
-                flash('Flow Removed', category='success')
-                return redirect(url_for('views.postPage'))
-            else:
-                flash('Flow Removing Error', category='error')
-                return redirect(url_for('views.postPage'))
+            tempData['input']["instructions"]["instruction"][0]["apply-actions"]["action"]= [{"order": 0,"drop-action": {}}]
+        if request.form.get('dstIP')!="None":
+            tempData['input']["match"]["ipv4-destination"] = request.form.get('dstIP')
+        if request.form.get('srcIP')!="None":
+            tempData['input']["match"]["ipv4-source"] = request.form.get('srcIP')
+        if request.form.get('dstMAC')!="None":
+            tempData['input']["match"]["ethernet-match"]["ethernet-source"]={"address":request.form.get('dstMAC')}
+        if request.form.get('srcMAC')!="None":
+            tempData['input']["match"]["ethernet-match"]["ethernet-destination"]={"address":request.form.get('srcMAC')}
+        if request.form.get('ipProtocol')!="None":
+            tempData['input']["match"]["ip-match"]={"ip-protocol": int(request.form.get('ipProtocol'))}
+        tempData['input']["node"]= "/opendaylight-inventory:nodes/opendaylight-inventory:node[opendaylight-inventory:id='"+ str(switch) +"']"
+        sendData = json.dumps(tempData)
+        response = requests.post(removeFlowUrl,auth=HTTPBasicAuth('admin', 'admin'), data=sendData,headers=headers)
+        if response.status_code ==200:
+            flash('Flow Removed', category='success')
+            return redirect(url_for('apis.showFlows'))
+        else:
+            flash('Flow Removing Error', category='error')
+            return redirect(url_for('apis.showFlows'))
     else:
         flash('Error', category='error')
-        return redirect(url_for('views.postPage'))
+        return redirect(url_for('apis.showFlows'))
 
 @apis.route('/show_flows', methods=['GET', 'POST'])
 @login_required
@@ -208,38 +207,37 @@ def showFlows():
             for i in data2["nodes"]["node"][switch]["flow-node-inventory:table"]:
                 if i["opendaylight-flow-table-statistics:flow-table-statistics"]["active-flows"] != 0:
                     for flows in i["flow"]:
-                        switchData.append([data2["nodes"]["node"][switch]["id"],i["opendaylight-flow-table-statistics:flow-table-statistics"]["active-flows"],flows["id"],flows["priority"]])
-                        # print(flows['id'],flows["priority"],flows["match"]["ethernet-match"])
-                        if "ethernet-match" in flows["match"]:
-                            temp=[]
-                            if "ethernet-source" in flows["match"]["ethernet-match"]:
-                                temp.append(flows["match"]["ethernet-match"]["ethernet-source"]["address"])
-                            else: 
-                                temp.append("None")
-                            if "ethernet-destination" in flows["match"]["ethernet-match"]:
-                                temp.append(flows["match"]["ethernet-match"]["ethernet-destination"]["address"])
-                            else: 
-                                temp.append("None")
-                            switchData[count].extend(temp) 
-                        else: switchData[count].extend(["None1","None2"])                             
-                        if "in-port" in flows["match"]:   
-                            # print(flows['id'],flows['match'])
-                            switchData[count].extend([flows["match"]["in-port"]]) 
-                            # switchData[switch].extend(["500"]) 
-                        elif "in-port" not in flows["match"]: 
-                            # print(flows['match'])
-                            switchData[count].extend(["None3"]) 
-                        if "ip-match" in flows["match"]:
-                            switchData[count].extend([flows["match"]["ip-match"]["ip-protocol"]]) 
-                        elif "ip-match" not in flows["match"]:
-                            switchData[count].extend(["None"]) 
-                        if "ipv4-destination" in flows["match"]:
-                            switchData[count].extend([flows["match"]["ipv4-destination"]]) 
-                        elif "ipv4-destination" not in flows["match"]:
-                            switchData[count].extend(["None"]) 
-                        if "ipv4-source" in flows["match"]:
-                            switchData[count].extend([flows["match"]["ipv4-source"]]) 
-                        elif "ipv4-source" not in flows["match"]:
-                            switchData[count].extend(["None"]) 
-                        count+=1
+                        if  flows["priority"] in range(1,100)  : 
+                            switchData.append([data2["nodes"]["node"][switch]["id"],flows["priority"]])
+                            # print(flows['id'],flows["priority"],flows["match"]["ethernet-match"])
+                            if "ethernet-match" in flows["match"]:
+                                temp=[]
+                                if "ethernet-source" in flows["match"]["ethernet-match"]:
+                                    temp.append(flows["match"]["ethernet-match"]["ethernet-source"]["address"])
+                                else: 
+                                    temp.append("None")
+                                if "ethernet-destination" in flows["match"]["ethernet-match"]:
+                                    temp.append(flows["match"]["ethernet-match"]["ethernet-destination"]["address"])
+                                else: 
+                                    temp.append("None")
+                                switchData[count].extend(temp) 
+                            else: switchData[count].extend(["None","None"])                              
+                            if "ip-match" in flows["match"]:
+                                switchData[count].extend([flows["match"]["ip-match"]["ip-protocol"]]) 
+                            elif "ip-match" not in flows["match"]:
+                                switchData[count].extend(["None"]) 
+                            if "ipv4-destination" in flows["match"]:
+                                switchData[count].extend([flows["match"]["ipv4-destination"]]) 
+                            elif "ipv4-destination" not in flows["match"]:
+                                switchData[count].extend(["None"]) 
+                            if "ipv4-source" in flows["match"]:
+                                switchData[count].extend([flows["match"]["ipv4-source"]]) 
+                            elif "ipv4-source" not in flows["match"]:
+                                switchData[count].extend(["None"]) 
+                            if "instructions" in flows:
+                                switchData[count].extend(["Allow"]) 
+                            elif "instructions" not in flows:
+                                switchData[count].extend(["Deny"]) 
+                            count+=1
+
     return render_template("flows.html", user=current_user, switchData=switchData , switchCount = nodesCount )
